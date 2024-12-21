f3c3696163b448c8b8feecb84066e5c3d75068ed
theussoares <matheusfs774@gmail.com>
2024-12-21 06:59:38 -0400

    fazendo alterações para o vercel

--- love/nuxt.config.ts
+++ love/nuxt.config.ts
@@ -2,6 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  ssr: false,
+  target: 'static', // Gera um site estático
+  build: {
+    outDir: '.output/public' // Diretório de saída para a construção estática
+  },
+  app: {
+    baseURL: '/' // (Se necessário, configure a baseURL conforme seu caminho de deploy)
+  },
 
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   css: ['@/assets/css/tailwind.css']
