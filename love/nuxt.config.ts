88531c4f6abc2c718fcbef52e6fe78864719f90b
theussoares <matheusfs774@gmail.com>
2024-12-21 06:51:49 -0400

    ativando o ssr

--- love/nuxt.config.ts
+++ love/nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  ssr: false,
 
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   css: ['@/assets/css/tailwind.css']
